2fdfbdbb9bbc3fa4c8b813cc1c31355e40593eba
Anouluck Phengvongsa <anouluck.phengvongsaa@gmail.com>
2023-08-25 17:55:29 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="app-header">
+        <h4> Just Tattoo</h4>
+      </header>
+      <div className="app-grid">
+        <div>123456 123456 123456</div>
+        <div>123456 123456 123456</div>
+        <div>123456 123456 123456</div>
+        <div>123456 123456 123456</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
